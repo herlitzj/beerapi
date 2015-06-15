dd0c027d0e685b48b6f92997a39a65a78c4c2ba4
herlitzj <herlitzj@gmail.com>
2015-06-15 23:16:58 +0000

    basic user model in place

--- server.js
+++ server.js
@@ -1,83 +1,31 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var beerModel = require('./models/beer');
+var beerController = require('./controllers/beer')
+var userController = require('./controllers/user')
 var app = express();
 var port = process.env.PORT || 8080;
 var router = express.Router();
 
+mongoose.connect('mongodb://localhost:27017/beerapi');
+
 app.use(bodyParser.urlencoded( {
     extended: true
 }));
-
 app.use('/api', router);
 
-mongoose.connect('mongodb://localhost:27017/beerapi');
-
-router.get('/', function(req, res) {
-    res.json({ message: "You are dangerously low on BEER." });
-});
-
-var beersRoute = router.route('/beers');
-
-beersRoute.post(function(req, res) {
-    var beer = new beerModel();
+router.route('/beers')
+    .post(beerController.postBeers)
+    .get(beerController.getBeers);
     
-    beer.name = req.body.name;
-    beer.type = req.body.type;
-    beer.quantity = req.body.quantity;
+router.route('/beers/:beer_id')
+    .get(beerController.getBeer)
+    .put(beerController.putBeer)
+    .delete(beerController.deleteBeer);
     
-    beer.save(function(err) {
-        if (err)
-            res.send(err);
-        
-        res.json({ message: 'Beer added to the locker!', data: beer });
-    });
-});
-
-beersRoute.get(function(req, res) {
-    beerModel.find(function(err, beers) {
-        if (err)
-            res.send(err);
-        
-        res.json(beers);
-    });
-});
-
-var beerRoute = router.route('/beers/:beer_id');
-
-beerRoute.get(function(req, res) {
-    beerModel.findById(req.params.beer_id, function(err, beer) {
-        if(err) 
-            res.send(err);
-            
-        res.json(beer);
-    });
-});
-
-beerRoute.put(function(req, res) {
-    beerModel.findById(req.params.beer_id, function(err, beer) {
-        if(err)
-            res.send(err);
-        
-        beer.quantity = req.body.quantity;
-        
-        beer.save(function(err) {
-            if(err)
-                res.send(err);
-                
-            res.json(beer);
-        });
-    });
-});
-
-beerRoute.delete(function(req, res) {
-    beerModel.findByIdAndRemove(req.params.beer_id, function(err) {
-        if(err)
-            res.send(err);
-        res.json({ message: "Beer removed from the locker" });
-    });
-});
+router.route('/users')
+    .post(userController.postUsers)
+    .get(userController.getUsers)
 
 app.listen(port);
 console.log("Listening on port " + port);
